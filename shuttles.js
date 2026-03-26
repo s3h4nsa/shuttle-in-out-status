bc2b8786c5cb337ab1711064b8f4aca724dadd8a
sehansa <98522960+s3h4nsa@users.noreply.github.com>
2026-03-26 15:41:35 +0530

    Update shuttles.js

--- shuttles.js
+++ shuttles.js
@@ -1,15 +1,42 @@
 const categories = [
+  { id: 'shuttle', name: 'Shuttle' },
   { id: 'pickup', name: 'Pickup Shuttle' },
   { id: 'branch', name: 'Branch Shuttle' },
-  { id: 'shuttle', name: 'Shuttle' },
 ];
 
 const shuttles = [
-  { id:1, name:'Matara Shuttle', category:'shuttle', template:'{name} Shuttle now in Polgasowita WH {time}.'},
-  { id:2, name:'Monaragala Shuttle', category:'shuttle', template:'{name} Shuttle now in Polgasowita WH {time}.'},
-  { id:3, name:'Baddegama Shuttle', category:'shuttle', template:'{name} Shuttle now in Polgasowita WH {time}.'},
-  { id:4, name:'Gampaha Pickup', category:'pickup', template:'{name} Shuttle now in Polgasowita WH {time}.'},
-  { id:5, name:'Negombo Pickup', category:'pickup', template:'{name} Shuttle now in Polgasowita WH {time}.'},
-  { id:6, name:'Polgasowita Branch', category:'branch', template:'{name} Shuttle now in Polgasowita WH {time}.'},
-  { id:7, name:'Gampaha Branch', category:'branch', template:'{name} Shuttle now in Polgasowita WH {time}.'},
+// Shuttle Shuttles
+  { id:1, name:'Baddegama', category:'shuttle', template:'{name} Shuttle now in Polgasowita WH {time}.'},
+  { id:2, name:'Balangoda', category:'shuttle', template:'{name} Shuttle now in Polgasowita WH {time}.'},
+  { id:3, name:'Chilaw', category:'shuttle', template:'{name} Shuttle now in Polgasowita WH {time}.'},
+  { id:4, name:'Gampaha', category:'shuttle', template:'{name} Shuttle now in Polgasowita WH {time}.'},
+  { id:5, name:'Homagama', category:'shuttle', template:'{name} Shuttle now in Polgasowita WH {time}.'},
+  { id:6, name:'Karawanella', category:'shuttle', template:'{name} Shuttle now in Polgasowita WH {time}.'},
+  { id:7, name:'Matara', category:'shuttle', template:'{name} Shuttle now in Polgasowita WH {time}.'},
+  { id:8, name:'Mathugama', category:'shuttle', template:'{name} Shuttle now in Polgasowita WH {time}.'},
+  { id:9, name:'Minuwangoda', category:'shuttle', template:'{name} Shuttle now in Polgasowita WH {time}.'},
+  { id:10, name:'Monaragala', category:'shuttle', template:'{name} Shuttle now in Polgasowita WH {time}.'},
+  { id:11, name:'Pettah', category:'shuttle', template:'{name} Shuttle now in Polgasowita WH {time}.'},
+  { id:12, name:'Piliyandala', category:'shuttle', template:'{name} Shuttle now in Polgasowita WH {time}.'},
+  
+// Pickup Shuttles
+  { id:13, name:'2-8', category:'pickup', template:'{name} Pickup Shuttle now in Polgasowita WH {time}.'},
+  { id:14, name:'9-15', category:'pickup', template:'{name} Pickup Shuttle now in Polgasowita WH {time}.'},
+  { id:15, name:'Balangoda', category:'pickup', template:'{name} Pickup Shuttle now in Polgasowita WH {time}.'},
+  { id:16, name:'Gampaha', category:'pickup', template:'{name} Pickup Shuttle now in Polgasowita WH {time}.'},
+  { id:17, name:'Homagama', category:'pickup', template:'{name} Pickup Shuttle now in Polgasowita WH {time}.'},
+  { id:18, name:'Kadawatha', category:'pickup', template:'{name} Pickup Shuttle now in Polgasowita WH {time}.'},
+  { id:19, name:'Matugama', category:'pickup', template:'{name} Pickup Shuttle now in Polgasowita WH {time}.'},
+  { id:20, name:'Minuwangoda', category:'pickup', template:'{name} Pickup Shuttle now in Polgasowita WH {time}.'},
+  { id:21, name:'Moratuwa', category:'pickup', template:'{name} Pickup Shuttle now in Polgasowita WH {time}.'},
+  { id:22, name:'Panadura', category:'pickup', template:'{name} Pickup Shuttle now in Polgasowita WH {time}.'},
+  { id:24, name:'Piliyandala', category:'pickup', template:'{name} Pickup Shuttle now in Polgasowita WH {time}.'},
+  { id:25, name:'Rajagiriya', category:'pickup', template:'{name} Pickup Shuttle now in Polgasowita WH {time}.'},
+  { id:26, name:'U Drop', category:'pickup', template:'{name} Pickup Shuttle now in Polgasowita WH {time}.'},
+  { id:27, name:'Wellampitiya', category:'pickup', template:'{name} Pickup Shuttle now in Polgasowita WH {time}.'},
+  { id:28, name:'Wattala', category:'pickup', template:'{name} Pickup Shuttle now in Polgasowita WH {time}.'},
+
+// Branch Shuttles
+  { id:29, name:'Kadawatha', category:'branch', template:'{name} Branch Shuttle now in Polgasowita WH {time}.'},
+  { id:30, name:'Piliyandala', category:'branch', template:'{name} Branch Shuttle now in Polgasowita WH {time}.'},
 ];
